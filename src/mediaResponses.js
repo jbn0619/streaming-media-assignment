1049f7b83cfbe6028fafc5f9d9313319c360f69d
jbn0619 <jbn5379@g.rit.edu>
2021-09-06 17:05:33 -0400

    Finished assignment!

--- src/mediaResponses.js
+++ src/mediaResponses.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
-const getParty = (request, response) => {
-  const file = path.resolve(__dirname, '../client/party.mp4');
+const loadMedia = (request, response, fileLocation, format) => {
+  const file = path.resolve(__dirname, fileLocation);
 
   fs.stat(file, (err, stats) => {
     if (err) {
@@ -14,44 +14,56 @@ const getParty = (request, response) => {
 
     let { range } = request.headers;
 
-  if (!range) {
-    range = 'bytes-0-';
-  }
+    if (!range) {
+      range = 'bytes-0-';
+    }
 
-  const positions = range.replace(/bytes=/, '').split('-');
+    const positions = range.replace(/bytes=/, '').split('-');
 
-  let start = parseInt(positions[0], 10);
+    let start = parseInt(positions[0], 10);
 
-  const total = stats.size;
-  const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+    const total = stats.size;
+    const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
 
-  if (start > end) {
-    start = end - 1;
-  }
+    if (start > end) {
+      start = end - 1;
+    }
 
-  const chunksize = (end - start) + 1;
+    const chunksize = (end - start) + 1;
 
-  response.writeHead(206, {
-    'Content-Range': `bytes ${start}-${end}/${total}`,
-    'Accept-Ranges': 'bytes',
-    'Content-Length': chunksize,
-    'Content-Type': 'video/mp4',
+    response.writeHead(206, {
+      'Content-Range': `bytes ${start}-${end}/${total}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': format,
+    });
+
+    const stream = fs.createReadStream(file, { start, end });
+
+    stream.on('open', () => {
+      stream.pipe(response);
+    });
+
+    stream.on('error', (streamErr) => {
+      response.end(streamErr);
+    });
+
+    return stream;
   });
+};
 
-  const stream = fs.createReadStream(file, { start, end });
+const getParty = (request, response) => {
+  loadMedia(request, response, '../client/party.mp4', 'video/mp4');
+};
 
-  stream.on('open', () => {
-    stream.pipe(response);
-  });
+const getBling = (request, response) => {
+  loadMedia(request, response, '../client/bling.mp3', 'audio/mp3');
+};
 
-  stream.on('error', (streamErr) => {
-    response.end(streamErr);
-  });
-
-  return stream;
-  });
-
-  
+const getBird = (request, response) => {
+  loadMedia(request, response, '../client/bird.mp4', 'video/mp4');
 };
 
 module.exports.getParty = getParty;
+module.exports.getBling = getBling;
+module.exports.getBird = getBird;
